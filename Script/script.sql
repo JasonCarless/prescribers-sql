d0939a87b1b23d8e40ba43e1880b9579d735ea73
Jason Carless <jtcarless@gmail.com>
2022-08-13 13:55:41 -0500

    after walkthrough

--- Script/script.sql
+++ Script/script.sql
@@ -1,20 +1,21 @@
 --1. a. Which prescriber had the highest total number of claims (totaled over all drugs)? Report the npi and the total number of claims.
-Select prescription.npi, total_claim_count
-from prescription
-join prescriber
-using(npi)
-where total_claim_count_ge65 is not null
-order by total_claim_count desc;
--- NPI 1912011792 had the most claims with 4538
- 
---b. Repeat the above, but this time report the nppes_provider_first_name, nppes_provider_last_org_name,  specialty_description, and the total number of claims.
-Select total_claim_count, nppes_provider_first_name, nppes_provider_last_org_name, specialty_description
+Select prescription.npi, SUM(total_claim_count) as total_claim
 from prescription
 join prescriber
 using(npi)
 where total_claim_count is not null
-order by total_claim_count desc;
---David Coffey in Family Practice had the most claims filed with 4538
+group by prescription.npi
+order by total_claim desc, prescription.npi;
+-- NPI 1881634483 had the most claims with 99707
+ 
+--b. Repeat the above, but this time report the nppes_provider_first_name, nppes_provider_last_org_name,  specialty_description, and the total number of claims.
+Select SUM(total_claim_count) as total_claim, nppes_provider_first_name, nppes_provider_last_org_name, specialty_description
+from prescription
+left join prescriber
+using(npi)
+group by nppes_provider_first_name, nppes_provider_last_org_name, specialty_description
+order by total_claim desc;
+--Bruce Pendley in Family Practice had the most claims filed with 99707
 
 --2. a. Which specialty had the most total number of claims (totaled over all drugs)?
 Select distinct prescriber.specialty_description, SUM(total_claim_count) as total_claims
@@ -26,11 +27,11 @@ order by total_claims desc;
 --Family practice has the most number of claims wtih 9,752,347
 
 --b. Which specialty had the most total number of claims for opioids?
-Select distinct prescriber.specialty_description, SUM(total_claim_count) as total_claims
+Select prescriber.specialty_description, SUM(total_claim_count) as total_claims
 from prescription
 left join prescriber
 using(npi)
-inner join drug
+left join drug
 on drug.drug_name = prescription.drug_name
 where drug.opioid_drug_flag = 'Y'
 group by specialty_description
@@ -43,29 +44,30 @@ order by total_claims desc;
 --d. **Difficult Bonus:** *Do not attempt until you have solved all other problems!* For each specialty, report the percentage of total claims by that specialty which are for opioids. Which specialties have a high percentage of opioids?
 
 --3. a. Which drug (generic_name) had the highest total drug cost?
-select generic_name, total_drug_cost
+select generic_name, sum(total_drug_cost) as drug_cost
 from drug
 inner join prescription
 using(drug_name)
-group by generic_name, total_drug_cost
-order by total_drug_cost desc;
--- "PIRFENIDONE" with a cost of $2,829,174.3
+group by generic_name
+order by drug_cost desc;
+-- INSULIN GLARGINE,HUM.REC.ANLOG with a cost of $104,264,066.35
 
 --b. Which drug (generic_name) has the hightest total cost per day? **Bonus: Round your cost per day column to 2 decimal places. Google ROUND to see how this works.
-select generic_name, (total_drug_cost/total_day_supply) as cost_per_day
+select generic_name, round (sum(total_drug_cost)/sum(total_day_supply),2) as cost_per_day
 from drug
-join prescription
+inner join prescription
 using(drug_name)
-group by generic_name, cost_per_day
+group by generic_name
 order by cost_per_day desc;
---"IMMUN GLOB G(IGG)/GLY/IGA OV50" has a cost per day of $7,141.1066666666666667
+--"C1 ESTERASE INHIBITOR" has a cost per day of $3,495.22
 
 --4. a. For each drug in the drug table, return the drug name and then a column named 'drug_type' which says 'opioid' for drugs which have opioid_drug_flag = 'Y', says 'antibiotic' for those drugs which have antibiotic_drug_flag = 'Y', and says 'neither' for all other drugs.
-Select drug_name,
+Select distinct drug_name,
 case when opioid_drug_flag = 'Y' then 'Opiod'
      when antibiotic_drug_flag = 'Y' then 'Antibiotic'
      else 'neither' end as drug_type
-     from drug;
+     from drug
+     order by drug_name;
 --b. Building off of the query you wrote for part a, determine whether more was spent (total_drug_cost) on opioids or on antibiotics. Hint: Format the total costs as MONEY for easier comparision.
 Select sum(total_drug_cost) as MONEY,
 case when opioid_drug_flag = 'Y' then 'Opiod'
@@ -75,21 +77,21 @@ case when opioid_drug_flag = 'Y' then 'Opiod'
      inner join prescription 
      on prescription.drug_name = drug.drug_name
      group by drug_type
-     order by drug_type desc;
+     order by MONEY desc;
 -- More was spent on opioids. $105,080,626.37 on opiods vs $38,435,121.26 on antibiotics
 
 --5. a. How many CBSAs are in Tennessee? **Warning:** The cbsa table contains information for all states, not just Tennessee.
-select *
+select distinct cbsaname
 from cbsa
 inner join fips_county
 on cbsa.fipscounty = fips_county.fipscounty 
 where state = 'TN';
---there are 42 cbas in Tennessee
+--there are 10 cbas in Tennessee
 
 --b. Which cbsa has the largest combined population? Which has the smallest? Report the CBSA name and total population.
 select cbsaname, SUM(population) as combined_pop
 from population
-left join cbsa
+inner join cbsa
 on population.fipscounty = cbsa.fipscounty
 group by cbsaname
 order by combined_pop desc;
@@ -116,7 +118,7 @@ where total_claim_count > 3000;
 select drug_name, total_claim_count,
  (select opioid_drug_flag
   from drug
-  where prescription.drug_name = drug.drug_name) as opioid
+  where prescription.drug_name = drug.drug_name)
 from prescription
 where total_claim_count > 3000;
 
