ecae36ccabd6d9ff3baa432987c352a4132256e0
Jason Carless <jtcarless@gmail.com>
2022-08-13 10:20:17 -0500

    Finished question 5B

--- Script/script.sql
+++ Script/script.sql
@@ -17,16 +17,25 @@ order by total_claim_count desc;
 --David Coffey in Family Practice had the most claims filed with 4538
 
 --2. a. Which specialty had the most total number of claims (totaled over all drugs)?
-Select total_claim_count, specialty_description
+Select distinct prescriber.specialty_description, SUM(total_claim_count) as total_claims
 from prescription
-join prescriber
+left join prescriber
 using(npi)
-group by specialty_description, total_claim_count
-order by total_claim_count desc;
---Family practice has the most number of claims
-
+group by specialty_description
+order by total_claims desc;
+--Family practice has the most number of claims wtih 9,752,347
 
 --b. Which specialty had the most total number of claims for opioids?
+Select distinct prescriber.specialty_description, SUM(total_claim_count) as total_claims
+from prescription
+left join prescriber
+using(npi)
+inner join drug
+on drug.drug_name = prescription.drug_name
+where drug.opioid_drug_flag = 'Y'
+group by specialty_description
+order by total_claims desc;
+--nurse practitioner had the most with 900,845 
     
 
 --c. **Challenge Question:** Are there any specialties that appear in the prescriber table that have no associated prescriptions in the prescription table?
@@ -52,12 +61,38 @@ order by cost_per_day desc;
 --"IMMUN GLOB G(IGG)/GLY/IGA OV50" has a cost per day of $7,141.1066666666666667
 
 --4. a. For each drug in the drug table, return the drug name and then a column named 'drug_type' which says 'opioid' for drugs which have opioid_drug_flag = 'Y', says 'antibiotic' for those drugs which have antibiotic_drug_flag = 'Y', and says 'neither' for all other drugs.
-
+Select drug_name,
+case when opioid_drug_flag = 'Y' then 'Opiod'
+     when antibiotic_drug_flag = 'Y' then 'Antibiotic'
+     else 'neither' end as drug_type
+     from drug;
 --b. Building off of the query you wrote for part a, determine whether more was spent (total_drug_cost) on opioids or on antibiotics. Hint: Format the total costs as MONEY for easier comparision.
+Select sum(total_drug_cost) as MONEY,
+SUM(case when opioid_drug_flag = 'Y' then total_drug_cost else 0 end) as opioid,
+SUM(case when antibiotic_drug_flag = 'Y' then total_drug_cost else 0 end) Antibiotic
+     from drug
+     left join prescription
+     on drug.drug_name = prescription.drug_name;
+-- More was spent on opioids. $105,080,626.37 on opiods vs $38,435,121.26 on antibiotics
 
 --5. a. How many CBSAs are in Tennessee? **Warning:** The cbsa table contains information for all states, not just Tennessee.
+select *
+from cbsa
+inner join fips_county
+on cbsa.fipscounty = fips_county.fipscounty 
+where state = 'TN';
+--there are 42 cbas in Tennessee
 
 --b. Which cbsa has the largest combined population? Which has the smallest? Report the CBSA name and total population.
+select cbsaname, SUM(population) as combined_pop
+from population
+left join cbsa
+on population.fipscounty = cbsa.fipscounty
+group by cbsaname
+order by combined_pop desc;
+-- "Nashville-Davidson--Murfreesboro--Franklin, TN" is the largest with a population of 1830410
+-- "Morristown, TN" is the smallest with 116352
+
 
 --c. What is the largest (in terms of population) county which is not included in a CBSA? Report the county name and population.
 
